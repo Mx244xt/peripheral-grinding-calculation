8356f458401c4f1f05a96555dd501741173ab2ef
Mx244xt <dev.se.masuda@gmail.com>
2024-02-22 14:51:59 +0900

    update: Change item list array to associative array

--- src/lib/selectItems.ts
+++ src/lib/selectItems.ts
@@ -1,237 +1,218 @@
-export const shapes = [
-  {
-    id: 1,
-    symbol: "R",
+export const shapes = {
+  "R": {
     value: "0",
     value2: "0",
     corner: "0",
   },
-  {
-    id: 2,
-    symbol: "S",
+  "S": {
     value: "90",
     value2: "90",
     corner: "4"
   },
-  {
-    id: 3,
-    symbol: "T",
+  "T": {
     value: "60",
     value2: "0",
     corner: "3"
   },
-  {
-    id: 4,
-    symbol: "C",
+  "C": {
     value: "80",
     value2: "100",
     corner: "4"
   },
-  {
-    id: 5,
-    symbol: "D",
+  "D": {
     value: "55",
     value2: "125",
     corner: "4"
   },
-  {
-    id: 6,
-    symbol: "V",
+  "V": {
     value: "35",
     value2: "145",
     corner: "4"
   },
-  {
-    id: 7,
-    symbol: "W",
+  "W": {
     value: "80",
     value2: "100",
     corner: "6"
   },
-  {
-    id: 99,
-    symbol: "その他",
-    value: "0",
-    value2: "0",
-    corner: "0"
+  "other": {
+    value: "other"
   },
-];
+};
 
-export const clearanceAngles = [
-  {
-    id: 1,
-    symbol: "A",
+export const clearanceAngles = {
+  "A": {
+
     value: "3",
   },
-  {
-    id: 2,
-    symbol: "B",
+  "B": {
     value: "5",
   },
-  {
-    id: 3,
-    symbol: "C",
+  "C": {
     value: "7",
   },
-  {
-    id: 4,
-    symbol: "D",
+  "D": {
     value: "15",
   },
-  {
-    id: 5,
-    symbol: "E",
+  "E": {
     value: "20",
   },
-  {
-    id: 6,
-    symbol: "F",
+  "F": {
     value: "25",
   },
-  {
-    id: 7,
-    symbol: "G",
+  "G": {
     value: "30",
   },
-  {
-    id: 8,
-    symbol: "N",
+  "N": {
     value: "0",
   },
-  {
-    id: 9,
-    symbol: "P",
+  "P": {
     value: "11",
   },
-];
+  "other": {
+    value: "other"
+  }
+};
 
-export const inscribedCircles = [
-  {
-    id: 1,
-    value: "3.97"
+export const inscribedCircles = {
+  "3.97": {
+    value: "3.97",
+    T: "06",
+    S: "03",
+    C: "03",
+    D: "04",
+    V: "",
+    R: "",
+    W: "",
   },
-  {
-    id: 2,
-    value: "4.76"
-  },
-  {
-    id: 3,
-    value: "5.56"
-  },
-  {
-    id: 4,
-    value: "6.0"
-  },
-  {
-    id: 5,
-    value: "6.35"
-  },
-  {
-    id: 6,
-    value: "7.94"
-  },
-  {
-    id: 7,
-    value: "8.0"
-  },
-  {
-    id: 8,
-    value: "9.525"
-  },
-  {
-    id: 9,
-    value: "10.0"
-  },
-  {
-    id: 10,
-    value: "12.0"
-  },
-  {
-    id: 11,
-    value: "12.70"
-  },
-  {
-    id: 12,
-    value: "15.875"
-  },
-  {
-    id: 13,
-    value: "16.0"
-  },
-  {
-    id: 14,
-    value: "19.05"
-  },
-  {
-    id: 15,
-    value: "20.0"
-  },
-  {
-    id: 16,
-    value: "22.225"
-  },
-  {
-    id: 17,
-    value: "25.0"
-  },
-  {
-    id: 18,
-    value: "25.40"
-  },
-  {
-    id: 19,
-    value: "31.75"
-  },
-  {
-    id: 20,
-    value: "32.0"
-  },
-];
+"4.76": {
+  value: "4.76"
+},
+"5.56": {
+  value: "5.56"
+},
+"6.0": {
+  value: "6.0"
+},
+"6.35": {
+  value: "6.35"
+},
+"7.94": {
+  value: "7.94"
+},
+"8.0": {
+  value: "8.0"
+},
+"9.525": {
+  value: "9.525"
+},
+"10.0": {
+  value: "10.0"
+},
+"12.0": {
+  value: "12.0"
+},
+"12.70": {
+  value: "12.70"
+},
+"15.875": {
+  value: "15.875"
+},
+"16.0": {
+  value: "16.0"
+},
+"19.05": {
+  value: "19.05"
+},
+"20.0": {
+  value: "20.0"
+},
+"22.225": {
+  value: "22.225"
+},
+"25.0": {
+  value: "25.0"
+},
+"25.40": {
+  value: "25.40"
+},
+"31.75": {
+  value: "31.75"
+},
+"32.0": {
+  value: "32.0"
+},
+"other": {
+  value: "other"
 
-export const thicknesses = [
-  {
-    id: 1,
-    symbol: "01",
+}
+};
+
+export const thicknesses = {
+  "01": {
     value: "1.59"
   },
-  {
-    id: 2,
-    symbol: "02",
+  "02": {
     value: "2.38"
   },
-  {
-    id: 3,
-    symbol: "T2",
+  "T2": {
     value: "2.78"
   },
-  {
-    id: 4,
-    symbol: "03",
+  "03": {
     value: "3.18"
   },
-  {
-    id: 5,
-    symbol: "T3",
+  "T3": {
     value: "3.97"
   },
-  {
-    id: 6,
-    symbol: "04",
+  "04": {
     value: "4.76"
   },
-  {
-    id: 7,
-    symbol: "06",
+  "06": {
     value: "6.35"
   },
-  {
-    id: 8,
-    symbol: "07",
+  "07": {
     value: "7.94"
   },
-  {
-    id: 9,
-    symbol: "09",
+  "09": {
     value: "9.52"
   },
-];
+  "other": {
+    value: "other"
+
+  }
+};
+
+export const cornerR = {
+  "00": {
+    value: "0"
+  },
+  "02": {
+    value: "0.2"
+  },
+  "04": {
+    value: "0.4"
+  },
+  "08": {
+    value: "0.8"
+  },
+  "12": {
+    value: "1.2"
+  },
+  "16": {
+    value: "1.6"
+  },
+  "20": {
+    value: "2.0"
+  },
+  "24": {
+    value: "2.4"
+  },
+  "28": {
+    value: "2.8"
+  },
+  "32": {
+    value: "3.2"
+  },
+  "other": {
+    value: "other"
+  },
+}
