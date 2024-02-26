770f0e89ed0b11eff893abbae91dbb8cd26ea978
Mx244xt <dev.se.masuda@gmail.com>
2024-02-26 11:30:41 +0900

    add: WebGL

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -4,18 +4,19 @@ type Props = {
   label: string;
   isAccordion: boolean;
   changAccordion: () => void;
+  gridNum?: boolean;
 }
 
-function Accordion({ children, isAccordion, changAccordion, label }: Props) {
+function Accordion({ children, isAccordion, changAccordion, label, gridNum }: Props) {
   return (
-    <section className="mb-5 border border-gray-200 rounded-xl dark:border-gray-700">
+    <section className="m-3 sm:m-5 mb-5 bg-white border border-gray-200 rounded-xl dark:bg-black dark:border-gray-700">
       <button type="button" className={`flex items-center justify-between w-full p-3 font-medium rtl:text-right text-gray-500 ${isAccordion ? "rounded-xl" : "rounded-t-xl"} focus:ring-1 focus:ring-gray-200 dark:focus:ring-gray-800  dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3`} onClick={() => changAccordion()}>
         <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">{label}</h2>
         <svg xmlns="http://www.w3.org/2000/svg" data-accordion-icon className={`w-3 h-3 ${isAccordion ? "rotate-0" : "rotate-180"} shrink-0`} aria-hidden="true" fill="none" viewBox="0 0 10 6">
           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5" />
         </svg>
       </button>
-      <div className={`${isAccordion && "hidden"} grid grid-cols-1 mb-5 sm:grid-cols-2`}>
+      <div className={`${isAccordion && "hidden"} grid grid-cols-1 mb-5 ${!gridNum && "sm:grid-cols-2"}`}>
         {children}
       </div>
     </section>
--- src/screens/Calculation.tsx
+++ src/screens/Calculation.tsx
@@ -5,6 +5,7 @@ import Select from "../components/Select";
 import Text from "../components/Text";
 import useCalc from "../hooks/useCalc";
 import { clearanceAngles, cornerR, inscribedCircles, shapes, thicknesses } from "../lib/selectItems";
+import WebGL from "../components/WebGL";
 
 function Calculation() {
 
@@ -13,34 +14,41 @@ function Calculation() {
     calc,
     handleChange,
   } = useCalc();
-  const { changThickness, changInscribedcircle, changReliefAngle, changReliefAngle3, changAngleLength, changShapes, changR } = handleChange;
+  const { changThickness, changInscribedcircle, changReliefAngle, changReliefAngle3, changAngleLength, changShapes, changR, changApexAngle } = handleChange;
 
-  const [isAccordion, setAccordion] = useState({ input: false, output: false });
+  const [isAccordion, setAccordion] = useState({ input: false, output: false, gl: false });
   function inputAccordion() {
     setAccordion({ ...isAccordion, input: !isAccordion.input });
   }
   function outputAccordion() {
     setAccordion({ ...isAccordion, output: !isAccordion.output });
   }
+  function WebGLAccordion() {
+    setAccordion({ ...isAccordion, gl: !isAccordion.gl });
+  }
 
   return (
     <>
       <Accordion label={"寸法入力"} isAccordion={isAccordion.input} changAccordion={() => inputAccordion()} >
-        <Select label={"形状"} items={shapes} setValue={changShapes} defaultValue={shapes["S"].value} />
-        <Select label={"内接円"} items={inscribedCircles} setValue={changInscribedcircle} defaultValue={inscribedCircles["9.525"].value} />
+        <Select label={"形状"} items={shapes} setValue={changShapes} defaultValue={"S"} optionSetValue={changApexAngle} />
+        <Select label={"内接円"} items={inscribedCircles} setValue={changInscribedcircle} defaultValue={"9.525"} />
         <Input label={"素材内接円"} num={dimensions.materialIncirecle} optionNum={(+dimensions.inscribedcircle + 0.2).toFixed(3)} setValue={handleChange.changMaterialIncirecle} min={0} max={36} step={0.1} />
-        <Select label={"逃げ角"} items={clearanceAngles} setValue={changReliefAngle} defaultValue={clearanceAngles["C"].value} />
-        <Select label={"Rサイズ"} items={cornerR} setValue={changR} defaultValue={cornerR["04"].value} />
-        <Select label={"厚み"} items={thicknesses} setValue={changThickness} defaultValue={thicknesses["03"].value} />
-        <Select label={"3番角"} items={clearanceAngles} setValue={changReliefAngle3} defaultValue={clearanceAngles["N"].value} />
+        <Select label={"逃げ角"} items={clearanceAngles} setValue={changReliefAngle} defaultValue={"C"} />
+        <Select label={"Rサイズ"} items={cornerR} setValue={changR} defaultValue={"04"} />
+        <Select label={"厚み"} items={thicknesses} setValue={changThickness} defaultValue={"03"} />
+        <Select label={"3番角"} items={clearanceAngles} setValue={changReliefAngle3} defaultValue={"N"} />
         {+dimensions.reliefAngle3 > 0 && <Input label={"ランド幅"} num={dimensions.angleLength} setValue={changAngleLength} min={0} max={36} step={0.01} />}
       </Accordion>
       <Accordion label={"計算結果"} isAccordion={isAccordion.output} changAccordion={() => outputAccordion()} >
         <Text label={"2番角仕上げ寸法"} anser={calc.reliefAngle()} description="内接円 ÷ 2 × cos(逃げ角)" />
-        <Text label={"ネガ当たり"} anser={calc.materialNegaHit()} description="(素材内接円 ÷ 2 × cos(逃げ角)) + (厚み × sin(逃げ角))" />
-        <Text label={"ポジ当たり"} anser={calc.materialPosiHit()} description="(素材内接円 ÷ 2 × cos(逃げ角))" />
-        <Text label={"コーナー高さ"} anser={calc.cornerHeight()} description={dimensions.shape === shapes["S"].value ? "(√2 -1) * (内接円 ÷ 2 - Rサイズ) " : dimensions.shape === shapes["T"].value ? "3 ÷ 2 × 内接円 - Rサイズ" : dimensions.shape === shapes["other"].value ? "" : "(1 ÷ sin(頂角 ÷ 2) - 1) × (内接円 ÷ 2 - Rサイズ)"} />
+        <Text label={"素材ネガ当たり"} anser={calc.materialNegaHit()} description="(素材内接円 ÷ 2 × cos(逃げ角)) + (厚み × sin(逃げ角))" />
+        <Text label={"B軸加工角度"} anser={calc.processingAngle()} description="内接円 ÷ 2 × cos(逃げ角)" />
+        <Text label={"素材ポジ当たり"} anser={calc.materialPosiHit()} description="(素材内接円 ÷ 2 × cos(逃げ角))" />
+        <Text label={"頂角コーナー高さ"} anser={calc.cornerHeight()} description={dimensions.shape === shapes["S"].value ? "(√2 -1) * (内接円 ÷ 2 - Rサイズ) " : dimensions.shape === shapes["T"].value ? "3 ÷ 2 × 内接円 - Rサイズ" : dimensions.shape === shapes["other"].value ? "" : "(1 ÷ sin(頂角 ÷ 2) - 1) × (内接円 ÷ 2 - Rサイズ)"} />
         {dimensions.reliefAngle3 !== "0" && dimensions.angleLength && dimensions.thickness && <Text label={"3番角仕上げ代"} anser={calc.reliefAngle3()} description="(内接円 ÷ 2 + (ランド幅 × (tan(3番角) - tan(逃げ角))) × cos(3番角)" />}
+      </Accordion>
+      <Accordion label={"図形"} isAccordion={isAccordion.gl} changAccordion={() => WebGLAccordion()} gridNum={true}>
+        <WebGL dimensions={dimensions}/>
       </Accordion>
     </>
   )
